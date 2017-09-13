2eb2d14a8c4e638cf92b1b2b97854b4ca0ada5b9
Aqib Ashef <aqib1952@gmail.com>
2017-09-13 18:18:53 +0600

    some minor fixes

--- inc/custom-meta-box-setup.php
+++ inc/custom-meta-box-setup.php
@@ -28,11 +28,6 @@ add_filter('rwmb_meta_boxes', 'add_metabox_for_projects_post_type');
  */
 
 function add_metabox_for_pages($meta_boxes){
-	// $gallery_terms = get_terms( array(
- //        'taxonomy'          => 'gallery',
- //        'hide_empty'        => false
- //    ));
-	// print_r($gallery_terms);
 	$meta_boxes[] = array(
 		'title'				=> __('Page Settings', 'snapie'),
 		'post_types'		=> 'page',
@@ -41,15 +36,14 @@ function add_metabox_for_pages($meta_boxes){
 				'id'			=> 'photo_archive_category',
 				'name'			=> __('Photo Archive Category', 'snapie'),
 				'desc'			=> __('This option will work only if the page template is set to Photo Archive.', 'snapie'),
-				'type'			=> 'select',
-				'options'       => array(
-                    'featured_image'        => __('Featured Image', 'snapie'),
-                    'gradient_overlay'      => __('Featured Image (With Gradient)', 'snapie'),
-                    'video_background'      => __('Video Background', 'snapie'),
-                    'text_rotator'          => __('Text Rotator', 'snapie'),
-                    'slider'                => __('Slider', 'snapie')
+				'type'			=> 'taxonomy',
+                'taxonomy'      => 'gallery',
+                'field_type'    => 'select',
+                'placeholder'   => 'Select a Category',
+                'query_args'    => array(
+                    'taxonomy'      => 'gallery',
+                    'hide_empty'    => true
                 )
-
 			)
 		)
 	);
