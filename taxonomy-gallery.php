63cceac2ed38d71a35a131ff48ffeec245a18b83
Aqib Ashef <aqib1952@gmail.com>
2017-09-15 13:23:52 +0600

    gallery page update

--- taxonomy-gallery.php
+++ taxonomy-gallery.php
@@ -9,41 +9,41 @@
 
 get_header(); ?>
 
-	<div id="primary" class="content-area aqib">
-		<main id="main" class="site-main" role="main">
-            <div class="col-md-8 gallery-wrapper">
-            <?php
-            if ( have_posts() ) : ?>
+    <div id="primary" class="content-area">
+        <main id="main" class="site-main" role="main">
+            <div class="photo-archive-container">
                 <?php
-                /* Start the Loop */
-                while ( have_posts() ) : the_post();
+                if ( have_posts() ) : ?>
+                    <div class="photo-container scroll center-v">
+                        <?php
+                        /* Start the Loop */
+                        while ( have_posts() ) : the_post();
 
-                    /*
-                     * Include the Post-Format-specific template for the content.
-                     * If you want to override this in a child theme, then include a file
-                     * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-                     */
-                    echo '<div class="col-md-4 preview">';
-                    get_template_part( 'template-parts/content', 'photos' );
-                    echo '</div>';
+                            /*
+                             * Include the Post-Format-specific template for the content.
+                             * If you want to override this in a child theme, then include a file
+                             * called content-___.php (where ___ is the Post Format name) and that will be used instead.
+                             */
+                            get_template_part( 'template-parts/content', 'photos' );
 
-                endwhile;
-                ?>
-                <?php
-                the_posts_pagination(array(
-                    'prev_text'     => '<i class="fa fa-angle-double-left" aria-hidden="true"></i>',
-                    'next_text'     => '<i class="fa fa-angle-double-right" aria-hidden="true"></i>'
-                ));
-            else :
-                ?>
-            <?php
-                get_template_part( 'template-parts/content', 'none' );
-                ?>
-                <?php
-            endif; ?>
+                        endwhile;
+                        ?>
+                    </div>
+                    <?php
+                    the_posts_pagination(array(
+                        'mid_size'			=> 2,
+                        'prev_text'			=> '<i class="fa fa-angle-double-left"></i>',
+                        'next_text'			=> '<i class="fa fa-angle-double-right"></i>'
+                    ));
+
+                else :
+
+                    get_template_part( 'template-parts/content', 'none' );
+
+                endif; ?>
             </div>
-		</main><!-- #main -->
-	</div><!-- #primary -->
+        </main><!-- #main -->
+    </div><!-- #primary -->
 
 <?php
 get_footer();
